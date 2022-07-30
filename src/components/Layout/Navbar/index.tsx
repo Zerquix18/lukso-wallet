1d4702d412d93b6097498920b01e17bb2b65e5bd
Luis Martinez <malbertoa_11@hotmail.com>
2022-07-30 15:49:12 -0400

    Add button to copy address to the clipboard

--- src/components/Layout/Navbar/index.tsx
+++ src/components/Layout/Navbar/index.tsx
@@ -1,17 +1,23 @@
-import { Button, Container, Dropdown, Heading, Icon, Navbar } from "react-bulma-components";
-import { useNavigate } from "react-router";
+import { Container, Dropdown, Heading, Icon, Navbar } from "react-bulma-components";
+import { toast } from 'bulma-toast';
 import { useAuthenticatedUser, useAuthentication } from "../../../hooks";
 
 function LayoutNavbar() {
-  const { authenticate, logout } = useAuthentication();
+  const { logout } = useAuthentication();
   const { address } = useAuthenticatedUser();
-
-  const navigate = useNavigate();
 
   const onDropdownSelect = (value: string) => {
     switch (value) {
-      case 'my-account':
-        navigate('/my-account');
+      case 'copy_address':
+        navigator.clipboard.writeText(address).then(() => {
+          toast({
+            message: 'Successfully added to the clipboard.',
+            type: 'is-success',
+            dismissible: true,
+            pauseOnHover: true,
+            position: 'bottom-right',
+          });
+        });
         break;
       case 'logout':
         if (! window.confirm('Are you sure you want to log out?')) {
@@ -37,6 +43,7 @@ function LayoutNavbar() {
             label={address.slice(0, 6) + '...' + address.slice(-4)}
             onChange={onDropdownSelect}
           >
+            <Dropdown.Item renderAs="a" value="copy_address">Copy Address</Dropdown.Item>
             <Dropdown.Item renderAs="a" value="logout">Logout</Dropdown.Item>
           </Dropdown>
         </Navbar.Container>
