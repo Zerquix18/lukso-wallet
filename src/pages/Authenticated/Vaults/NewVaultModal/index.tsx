a323f2cecafa7351c0822cb5a6fb4d11952a9219
Luis Martinez <malbertoa_11@hotmail.com>
2022-08-22 17:45:02 -0400

    Finish function to create vaults

--- src/pages/Authenticated/Vaults/NewVaultModal/index.tsx
+++ src/pages/Authenticated/Vaults/NewVaultModal/index.tsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import { Button, Modal } from "react-bulma-components";
 
-import LSP9Vault from '@lukso/lsp-smart-contracts/artifacts/LSP9Vault.json';
 import Web3 from "web3";
+import ERC725, { ERC725JSONSchema } from "@erc725/erc725.js";
+import LSP9Vault from '@lukso/lsp-smart-contracts/artifacts/LSP9Vault.json';
+import LSP10ReceivedVaults from '@erc725/erc725.js/schemas/LSP10ReceivedVaults.json';
+import LSP0ERC725Account from '@lukso/lsp-smart-contracts/artifacts/UniversalProfile.json';
 
 import { useAuthenticatedUser } from "../../../../hooks";
+import { IPFS_GATEWAY_API_BASE_URL } from "../../../../constants";
 
 interface NewVaultModalProps {
   onClose: () => void;
@@ -23,11 +27,55 @@ function NewVaultModal({ onClose }: NewVaultModalProps) {
       const web3 = new Web3(window.ethereum);
       const LSP9VaultAbi = LSP9Vault.abi as any;
       const vaultContract = new web3.eth.Contract(LSP9VaultAbi);
-      await vaultContract.deploy({
+      const newContract = await vaultContract.deploy({
         data: LSP9Vault.bytecode,
         arguments: [address],
       }).send({ from: address });
-      window.location.reload();
+
+      const newContractAddress = newContract.options.address;
+
+      ///////// ---
+
+      const options = {
+        ipfsGateway: IPFS_GATEWAY_API_BASE_URL,
+      };
+      const erc725LSP12IssuedAssets = new ERC725(
+        LSP10ReceivedVaults as ERC725JSONSchema[],
+        address,
+        web3.currentProvider,
+        options
+      );
+
+      const LSP9Vaults = await erc725LSP12IssuedAssets.getData('LSP10Vaults[]');
+      (LSP9Vaults.value as string[]).push(newContractAddress);
+      
+      const LSP9VaultsLength = (LSP9Vaults.value as string[]).length;
+      const LSP9InterfaceId = '0x8c1d44f6'; // https://docs.lukso.tech/standards/smart-contracts/interface-ids/
+
+      const encodedErc725Data = erc725LSP12IssuedAssets.encodeData([
+        {
+          keyName: 'LSP10Vaults[]',
+          value: LSP9Vaults.value,
+        },
+        {
+          keyName: 'LSP10VaultsMap:<address>',
+          dynamicKeyParts: newContractAddress,
+          value: [LSP9InterfaceId, String(LSP9VaultsLength - 1)],
+        },
+      ]);
+
+      console.log(encodedErc725Data);
+
+      const LSP0ERC725AccountAbi = LSP0ERC725Account.abi as any;
+      const profileContract = new web3.eth.Contract(LSP0ERC725AccountAbi, address);
+      const receipt = await profileContract.methods['setData(bytes32[],bytes[])'](
+        encodedErc725Data.keys,
+        encodedErc725Data.values
+      ).send({ from: address });
+
+      console.log(receipt);
+
+      // window.location.reload();
     } catch (e) {
       console.log(e);
     } finally {
--- src/pages/Authenticated/Vaults/index.tsx
+++ src/pages/Authenticated/Vaults/index.tsx
@@ -1,6 +1,32 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Button, Heading } from "react-bulma-components";
+
+import LSP10ReceivedVaults from '@erc725/erc725.js/schemas/LSP10ReceivedVaults.json';
+import ERC725, { ERC725JSONSchema } from "@erc725/erc725.js";
+
+import { DEFAULT_CONFIG, DEFAULT_PROVIDER } from "../../../constants";
+import { useAuthenticatedUser } from "../../../hooks";
+
 import NewVaultModal from "./NewVaultModal";
+
+function Vaults() {
+  const { address } = useAuthenticatedUser();
+
+  const erc725Ref = useRef(new ERC725(LSP10ReceivedVaults as ERC725JSONSchema[], address, DEFAULT_PROVIDER, DEFAULT_CONFIG));
+  const erc725 = erc725Ref.current;
+
+  const fetchVaults = async () => {
+    const data = await erc725.fetchData(['LSP10Vaults[]']);
+    console.log(data);
+    return [];
+  };
+
+  useEffect(() => {
+    fetchVaults();
+  }, [fetchVaults]);
+
+  return <></>;
+}
 
 function VaultsWrapper() {
   const [adding, setAdding] = useState(false);
@@ -19,6 +45,8 @@ function VaultsWrapper() {
       <div style={{ textAlign: 'right', marginBottom: 10 }}>
         <Button color="primary" onClick={toggleAdding}>Add Vault</Button>
       </div>
+
+      <Vaults />
 
       { adding && (
         <NewVaultModal onClose={toggleAdding} />
