0911a78bb13da1304d68209ee34c0bbc63d23125
Luis Martinez <malbertoa_11@hotmail.com>
2022-08-22 20:57:30 -0400

    change theme

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 
-import 'bulma/css/bulma.min.css';
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
